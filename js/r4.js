c2e525e5dad225fa7412cb8b659034f331de9034
ag171980 <ag171980@gmail.com>
2022-01-03 23:40:22 -0300

    Subo modif en r4.js

--- js/r4.js
+++ js/r4.js
@@ -3,7 +3,7 @@ let weather = document.getElementById("search-weather")
 let tabla = document.getElementById("table-weather")
 
 document.getElementById("btn-search").addEventListener("click", () => {
-    fetch(`http://api.weatherstack.com/current?access_key=81a63c7a86b1c1d629bfb564ceb98227&query=${weather.value}`)
+    fetch(`https://api.weatherstack.com/current?access_key=81a63c7a86b1c1d629bfb564ceb98227&query=${weather.value}`)
         .then(response => response.json())
         .then(function (data) {
             console.log(data)
